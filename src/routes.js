afc81042960a59df72b5679579f31acc2c42b29c
mandellasly1 <akpomah_nelson@yahoo.com>
2026-07-26 23:23:56 +0100

    Repeating the Process: Inserting New Service Projects

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
 import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm, processEditOrganizationForm } from './controllers/organizations.js';
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
@@ -34,6 +34,9 @@ router.get('/new-organization', showNewOrganizationForm );
 router.get('/edit-organization/:id', showEditOrganizationForm );
 
 
+// Route for new project page
+router.get('/new-project', showNewProjectForm);
+
 // error-handling routes
 router.get('/test-error', testErrorPage);
 
@@ -46,6 +49,9 @@ router.post('/new-organization', organizationValidation, processNewOrganizationF
 // Route to handle the edit organization form submission
 router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 
+
+// Route to handle new project form submission
+router.post('/new-project', projectValidation, processNewProjectForm);
 
 
 export default router;
