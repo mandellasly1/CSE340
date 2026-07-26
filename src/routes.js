9dfd0895ee5abd8800098d33722c88654a955c3b
mandellasly1 <akpomah_nelson@yahoo.com>
2026-07-26 18:52:10 +0100

    Updating Data

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation } from './controllers/organizations.js';
+import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm, processEditOrganizationForm } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -18,16 +18,20 @@ router.get('/categories', showCategoriesPage);
 
 
 // Route for organization details page
-router.get('/organization/:id', showOrganizationDetailsPage);
+router.get('/organization/:id', showOrganizationDetailsPage );
 
 // Route for project details page
-router.get('/project/:id', showProjectDetailsPage);
+router.get('/project/:id', showProjectDetailsPage );
 
 // Route for category details
-router.get('/category/:id', showCategoryDetailsPage);
+router.get('/category/:id', showCategoryDetailsPage );
 
 // Route for new organization page
-router.get('/new-organization', showNewOrganizationForm, );
+router.get('/new-organization', showNewOrganizationForm );
+
+
+// Route to display the edit organization form
+router.get('/edit-organization/:id', showEditOrganizationForm );
 
 
 // error-handling routes
@@ -38,6 +42,9 @@ router.get('/test-error', testErrorPage);
 // Route to handle new organization form submission
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
 
+
+// Route to handle the edit organization form submission
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 
 
 
